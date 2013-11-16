6390c2b52192c5343ff230232f1b5057705aa269
Andrée Ekroth <andree.ekroth@gmail.com>
2013-11-17 00:36:08 +0100

    add bootstrap. add form data

--- play/app/controllers/Application.scala
+++ play/app/controllers/Application.scala
@@ -2,14 +2,37 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formats._
+
+case class Bunny(x: Int, y: Int, w: Float)
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+  val bunnyForm = Form(
+    mapping(
+      "x" -> number,
+      "y" -> number,
+      "w" -> of[Float]
+    )(Bunny.apply)(Bunny.unapply)
+  )
+
+
+  def index = register
+
 
   def teddies(i: Int) = Action {
     Ok(views.html.teddies(i))
+  }
+
+
+  def register = Action {
+    Ok(views.html.register(bunnyForm))
+  }
+
+  def submit = Action { implicit request =>
+    val bunny = bunnyForm.bindFromRequest.get
+    Ok(s"Bunny: $bunny")
   }
 }
